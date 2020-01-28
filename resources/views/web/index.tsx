6b7a7acb4d988d05ad5f9d61a3816947e6ed0700
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2020-01-27 20:06:21 -0400

    Atualizado pacotes

--- resources/views/web/index.tsx
+++ resources/views/web/index.tsx
@@ -2,13 +2,22 @@ import * as React from 'react';
 import Layout from '../layouts/default';
 
 interface Props {
-    count: number;
+    numeroRegistros: number;
+    pessoas: Pessoa[];
+}
+
+interface Pessoa {
+    id: number;
+    name: string;
+    idade: number;
 }
 
 export default (props: Props) => {
     return (
         <Layout title='Home'>
-            <div>Welcome to the home page {props.count} Visits</div>
+            <div>Welcome to the home page {props.numeroRegistros} Visits</div>
+
+            {props.pessoas.map(a => <h1>a.name</h1>)}
         </Layout>
     );
 };
